73d2553e297c193abba7be9ba8a8b3b8c2a41d43
Gabriel Cerutti <ceruttigabriel@gmail.com>
2024-02-23 16:25:37 -0300

    Style update + lazy loading

--- host-app/src/App.jsx
+++ host-app/src/App.jsx
@@ -1,19 +1,22 @@
 import "./App.scss";
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import useSession from "./store";
 //import AppLoader from "./AppLoader";
-import MicroApp1 from "microApp1/App";
+//import MicroApp1 from "microApp1/App";
 import useStore from "microApp1/store";
-import MicroApp2 from "microApp2/App";
+//import MicroApp2 from "microApp2/App";
 
 function App() {
   const [count, setCount] = useStore();
   const [user, setUser] = useSession();
-  const [localUser, setLocalUser] = useState('');
+  const [localUser, setLocalUser] = useState("");
   const [currentApp, setCurrentApp] = useState("microApp1");
 
+  const MicroApp1 = React.lazy(() => import("microApp1/App"));
+  const MicroApp2 = React.lazy(() => import("microApp2/App"));
+
   useEffect(() => {
-    const user = localStorage.getItem('user');
+    const user = localStorage.getItem("user");
     if (user) {
       setUser(user);
     }
@@ -22,47 +25,64 @@ function App() {
   return (
     <div className="host-app">
       <div className="input-container">
-        <input 
-          type="text" 
-          onChange={(e) => setLocalUser(e.target.value)} 
+        <input
+          type="text"
+          onChange={(e) => setLocalUser(e.target.value)}
           className="input-field"
         />
-        <button 
-          onClick={() => { localStorage.setItem('user', localUser); setUser(localUser); }}
+        <button
+          onClick={() => {
+            localStorage.setItem("user", localUser);
+            setUser(localUser);
+          }}
           className="input-button"
         >
           Sign in
         </button>
-        
+
         <h3 className="logged-user">
-          {user === '' && 'No user logged in'}
-          {user !== '' && 'Logged user ' + user}
+          {user === "" && "No user logged in"}
+          {user !== "" && "Logged user " + user}
         </h3>
       </div>
 
       <h1>Host App</h1>
 
       <div className="buttons-container">
-        <button className="button" onClick={() => setCurrentApp("microApp1")}>Micro App 1</button>
-        <button className="button" onClick={() => setCurrentApp("microApp2")}>Micro App 2</button> 
+        <button className="button" onClick={() => setCurrentApp("microApp1")}>
+          Micro App 1
+        </button>
+        <button className="button" onClick={() => setCurrentApp("microApp2")}>
+          Micro App 2
+        </button>
       </div>
-      
+
       <h4> # This counter state is shared from micro-app-1 to host # </h4>
       <div className="buttons-container">
-        <button className="button" onClick={() => setCount((count) => count + 1)}>
-            Count is {count}
-        </button>        
+        <button
+          className="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
       </div>
-      
+
       <div className="micro-app-container">
-  
         <div className="micro-app">
-          {currentApp === "microApp1" &&  <MicroApp1></MicroApp1>}
+          {currentApp === "microApp1" && (
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <MicroApp1></MicroApp1>
+            </React.Suspense>
+          )}
           {/* <AppLoader endpoint="microApp1/App" /> */}
           {/* <MicroApp1></MicroApp1> */}
         </div>
         <div className="micro-app">
-          {currentApp === "microApp2" && <MicroApp2></MicroApp2>}
+          {currentApp === "microApp2" && (
+            <React.Suspense fallback={<div>Loading...</div>}>
+              <MicroApp2></MicroApp2>
+            </React.Suspense>
+          )}
         </div>
       </div>
     </div>
