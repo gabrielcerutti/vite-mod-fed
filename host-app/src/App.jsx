55ff6c8b17e80414bb3db48b73c9314bbeebbe50
Gabriel Cerutti <ceruttigabriel@gmail.com>
2024-02-28 09:44:09 -0300

    Added react router to the host app

--- host-app/src/App.jsx
+++ host-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import useSession from "./store";
 import AppLoader from "./AppLoader";
 import useStore from "microApp1/store";
@@ -8,7 +9,6 @@ function App() {
   const [count, setCount] = useStore();
   const [user, setUser] = useSession();
   const [localUser, setLocalUser] = useState("");
-  const [currentApp, setCurrentApp] = useState("microApp1");
 
   useEffect(() => {
     const user = localStorage.getItem("user");
@@ -18,63 +18,59 @@ function App() {
   }, [setUser]);
 
   return (
-    <div className="host-app">
-      <div className="input-container">
-        <input
-          type="text"
-          onChange={(e) => setLocalUser(e.target.value)}
-          className="input-field"
-        />
-        <button
-          onClick={() => {
-            localStorage.setItem("user", localUser);
-            setUser(localUser);
-          }}
-          className="input-button"
-        >
-          Sign in
-        </button>
+    <Router>
+      <div className="host-app">
+        <div className="input-container">
+          <input
+            type="text"
+            onChange={(e) => setLocalUser(e.target.value)}
+            className="input-field"
+          />
+          <button
+            onClick={() => {
+              localStorage.setItem("user", localUser);
+              setUser(localUser);
+            }}
+            className="input-button"
+          >
+            Sign in
+          </button>
 
-        <h3 className="logged-user">
-          {user === "" && "No user logged in"}
-          {user !== "" && "Logged user " + user}
-        </h3>
-      </div>
-
-      <h1>Host App</h1>
-
-      <div className="buttons-container">
-        <button className="button" onClick={() => setCurrentApp("microApp1")}>
-          Micro App 1
-        </button>
-        <button className="button" onClick={() => setCurrentApp("microApp2")}>
-          Micro App 2
-        </button>
-      </div>
-
-      <h4> # This counter state is shared from micro-app-1 to host # </h4>
-      <div className="buttons-container">
-        <button
-          className="button"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </div>
-
-      <div className="micro-app-container">
-        <div className="micro-app">
-          {currentApp === "microApp1" && (
-            <AppLoader module="microApp1" />
-          )}
+          <h3 className="logged-user">
+            {user === "" && "No user logged in"}
+            {user !== "" && "Logged user " + user}
+          </h3>
         </div>
-        <div className="micro-app">
-          {currentApp === "microApp2" && (
-            <AppLoader module="microApp2" />
-          )}
+
+        <h1>Host App</h1>
+
+        <div className="buttons-container">
+          <Link className="button" to="/microApp1">
+            Micro App 1
+          </Link>
+          <Link className="button" to="/microApp2">
+            Micro App 2
+          </Link>
+        </div>
+
+        <h4> # This counter state is shared from micro-app-1 to host # </h4>
+        <div className="buttons-container">
+          <button
+            className="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Count is {count}
+          </button>
+        </div>
+
+        <div className="micro-app-container">
+          <Routes>
+            <Route path="/microApp1" element={<AppLoader module="microApp1" />} />
+            <Route path="/microApp2" element={<AppLoader module="microApp2" />} />
+          </Routes>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
